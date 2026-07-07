c414a0abfda2b3ce43a5872e87402bf6f7f8606a
Jan Rzeszutek <rzeszut34@gmail.com>
2026-07-07 19:37:09 +0200

    adding gitignore

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include<stdio.h>
+#include<stdbool.h>
+
+
+#define NUMBER_OF_PRIMES 1000
 
 int main()
 {
-  printf("lets start with primes");
+  int i = 2;
+  int primesCount = 0;
+  int primes[NUMBER_OF_PRIMES];
+  while(primesCount < NUMBER_OF_PRIMES){
+    bool isPrime = true;
+    for(int j = 2; j < i/2+1; ++j){
+      for(int temp_j = j; temp_j <= i; temp_j +=j){
+        if(temp_j == i){
+          isPrime = false;
+          continue;
+        }
+      }
+      if(isPrime == false){
+        continue;
+      }
+    }
+    if(isPrime){
+      primes[primesCount] = i;
+      ++primesCount;
+    }
+    ++i;
+  }
+
+  for (int index = 0; index < NUMBER_OF_PRIMES; ++index){
+    printf("%d ", primes[index]);
+  }
   return 0;
 }
